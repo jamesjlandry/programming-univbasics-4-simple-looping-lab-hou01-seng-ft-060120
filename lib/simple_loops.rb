9bf4273581d08c0710b29830d73090a57fe163b8
James Landry <james.j.landry@outlook.com>
2020-05-19 15:07:52 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
 # Write your methods here
+def loop_message_five_times(message)
+  repeater = 0
+  while repeater < 5 do
+    puts "#{message}"
+    repeater += 1
+  end
+end
+
+def loop_message_n_times(message, number)
+  count = 0
+while count < number do
+  puts message
+  count += 0
+  end
+end
